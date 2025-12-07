50a0653d6a584c642bdcf7b911c2c3c12ce3af37
Baba05206 <babatunde.yusuf@outlook.com>
2025-12-07 18:24:09 +0000

    Updated code to reflected all 6 improvements recommended

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,28 +1,33 @@
+// Constants for time conversion
+const SECONDS_PER_MINUTE = 60;
+const MILLISECONDS_PER_SECOND = 1000;
+
 let intervalId;
 
 function setAlarm() {
   const input = document.getElementById("alarmSet");
-  let time = Number(input.value);
-
   const heading = document.getElementById("timeRemaining");
+
+  let totalSeconds = Number(input.value);
+
+  // Input validation: check for invalid or non-positive numbers
+  if (isNaN(totalSeconds) || totalSeconds <= 0) {
+    heading.innerText = "Please enter a valid positive number of seconds";
+    return;
+  }
 
   clearInterval(intervalId);
 
-  function format(num) {
-    if (num < 10) return "0" + num;
-    return num;
-  }
-
   heading.innerText =
     "Time Remaining: " +
-    format(Math.floor(time / 60)) +
+    String(Math.floor(totalSeconds / SECONDS_PER_MINUTE)).padStart(2, "0") +
     ":" +
-    format(time % 60);
+    String(totalSeconds % SECONDS_PER_MINUTE).padStart(2, "0");
 
   intervalId = setInterval(() => {
-    time--;
+    totalSeconds--;
 
-    if (time <= 0) {
+    if (totalSeconds <= 0) {
       heading.innerText = "Time Remaining: 00:00";
       clearInterval(intervalId);
       playAlarm();
@@ -31,10 +36,10 @@ function setAlarm() {
 
     heading.innerText =
       "Time Remaining: " +
-      format(Math.floor(time / 60)) +
+      String(Math.floor(totalSeconds / SECONDS_PER_MINUTE)).padStart(2, "0") +
       ":" +
-      format(time % 60);
-  }, 1000);
+      String(totalSeconds % SECONDS_PER_MINUTE).padStart(2, "0");
+  }, MILLISECONDS_PER_SECOND);
 }
 // DO NOT EDIT BELOW HERE
 
